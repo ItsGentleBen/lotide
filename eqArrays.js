da3ac4a053b2dc1cc015b9ae46dad4edc83021cd
Ben Stolb <benstolb@protonmail.com>
2022-10-18 17:16:58 -0700

    added eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArr, seconArr) {
+  if (firstArr.length !== seconArr.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== seconArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => should PASS
+
