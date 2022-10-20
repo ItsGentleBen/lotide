4756f70bbf0613ea807137f78a3325baa7984631
Ben Stolb <benstolb@protonmail.com>
2022-10-19 20:26:50 -0700

    added letterPositions.js for finding position of each letter

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const eqArrays = function(firstArr, seconArr) {
+  if (firstArr.length !== seconArr.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== seconArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArr, secondArr) {
+  if (eqArrays(firstArr, secondArr)) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArr} !== ${secondArr}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      results[sentence[i]] ? results[sentence[i]].push(i) : results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
