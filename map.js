8db1d4c13064e07ff7d23561f74285cc28a11ddf
Ben Stolb <benstolb@protonmail.com>
2022-10-20 15:49:25 -0700

    Added map.js

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(firstArr, seconArr) {
+  if (firstArr.length !== seconArr.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== seconArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArr, secondArr) {
+  if (eqArrays(firstArr, secondArr)) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArr} !== ${secondArr}`);
+  }
+};
+
+
+
+const map = (array, callback) =>{
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1)
+
+
