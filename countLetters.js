62981eff7af90fd6f52fe7fa3c5a541f1f3fc77b
Ben Stolb <benstolb@protonmail.com>
2022-10-19 17:51:20 -0700

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+
+};
+const countLetters = function(inputStrings) {
+  let output = {};
+  for (let character of inputStrings) {
+    if (character !== " ") {
+      if (output[character]) {
+        output[character] += 1;
+      } else {
+        output[character] = 1;
+      }
+    }
+  }
+  return output;
+};
+
+console.log(countLetters("lightouse in the house"));
